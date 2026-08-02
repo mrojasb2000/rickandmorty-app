a8123c4e9297bf58a14c1aacad8d4ccc05eb80a2
Mauricio Rojas <mrojasb2000@gmail.com>
2026-08-02 16:17:05 -0400

    feat: add list characters molecules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client';
+import List from './components/molecules/List.js';
+
 
 function App() {
   return (
     <div>
       <h1>Hello, Rick & Morty!</h1>
+      <List />
     </div>
   );
 }
