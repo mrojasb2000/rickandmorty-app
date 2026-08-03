b1bd9c90c7db962d309a84c8844c69a25dc5b9d5
Mauricio Rojas <mrojasb2000@gmail.com>
2026-08-02 21:47:34 -0400

    refactor: style card character

--- src/components/molecules/Character.js
+++ src/components/molecules/Character.js
@@ -1,10 +1,10 @@
 function Character(character) {
   return (
-    <div className="card" style={{ width: '200px' }}>
+    <div className="card" style={{ width: '200px', height: '280px' }}>
       <img src={character.image} alt={character.name} className="card-img-top" />
       <div className="card-body">
-        <h5 className="card-title">{character.name}</h5>
-        <p className="card-text">{`Origin: ${character.origin}`}</p>
+        <h5 className="card-title">{character.name.substring(0, 15)}...</h5>
+        <p className="card-text">{`Origin: ${character.origin.substring(0, 12)}...`}</p>
       </div>
     </div>
   );
